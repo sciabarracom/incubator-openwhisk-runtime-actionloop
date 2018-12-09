480a1c11c8bdff8254475e17f74cc3e8c47cb8df
Michele Sciabarra <sciabarracom@sciabarra.com>
2018-12-09 15:28:51 +0100

    adding report

---
+++ benchmark/report.py
@@ -0,0 +1,120 @@
+from __future__ import print_function
+import fileinput
+import json
+import copy
+
+legends = {
+    "init": "",
+    "run": ""
+}
+
+groups = { }
+
+def process_line(info, time):
+    global legends, groups
+    i = info.split(" ")
+    t = time.split(" ")
+    time = float(t[0])
+    key = i[0]
+    label = "%s %s" % (i[1], i[2])
+    legend = "%s_%s" % (i[0], i[3])
+    legends[key] = legend
+    if not label in groups:
+        groups[label] = { "init":0, "run":0}
+    groups[label][key] = time
+
+
+def process_input():
+    odd = True
+    buf = ""
+    for line in fileinput.input():
+        line = line.strip()
+        if odd:
+            buf = line
+        else:
+            process_line(buf, line)
+            buf = ""
+        odd = not odd
+
+sampleData = {
+    "labels": [],
+    "datasets": [{
+	    "label": 'init',
+	    "backgroundColor": '#4dc9f6',
+	    "borderColor": 'cyan',
+		"borderWidth": 1,
+		"data": []
+	}, {
+	    "label": 'run',
+	    "backgroundColor": '#00a950',
+	    "borderColor": 'green',
+	    "borderWidth": 1,
+	    "data": []
+	}]
+}
+
+def make_bar_data(keys):
+    barData = copy.deepcopy(sampleData)
+    for label in keys:
+        barData["labels"].append(label)
+        barData["datasets"][0]["data"].append(groups[label]["init"])
+        barData["datasets"][1]["data"].append(groups[label]["run"])
+    barData["datasets"][0]["label"] = legends["init"]
+    barData["datasets"][1]["label"] = legends["run"]
+    return json.dumps(barData)
+
+init_json = ""
+run_json = ""
+
+def post_process():
+    global init_json, run_json
+    m = groups
+    ls = [ (m[k]['init'], k,m[k]) for k in m]
+    ls.sort(key=lambda x: x[0])
+    keys = [k[1] for k in ls]
+    #print(keys)
+    init_json = make_bar_data(keys)
+    ls1 = [ (m[k]['run'], k,m[k]) for k in m]
+    ls1.sort(key=lambda x: x[0])
+    keys1 = [k[1] for k in ls]
+    run_json = make_bar_data(keys1)
+
+#print(json.dumps(result))
+def html():
+    return """
+<html>
+<head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.js"></script>
+</head>
+<body>
+</body>
+<h1>Run</h1>
+<canvas id="runChart"></canvas>
+<h1>Init</h1>
+<canvas id="initChart"></canvas>
+<script>
+var barDataRun = %s;
+var barDataInit = %s;
+Chart.scaleService.updateScaleDefaults('linear', {
+    ticks: { min: 0 }
+});
+var ctx1 = document.getElementById('runChart').getContext('2d');
+var chart1 = new Chart(ctx1, {
+    type: 'horizontalBar',
+    data: barDataRun
+});
+var ctx2 = document.getElementById('initChart').getContext('2d');
+var chart1 = new Chart(ctx2, {
+    type: 'horizontalBar',
+    data: barDataInit
+});
+</script>
+</html>
+""" %  (run_json, init_json)
+
+
+process_input()
+#print(groups)
+post_process()
+print(html())
+#print(json.dumps(barData))
--- benchmark/test.sh
+++ benchmark/test.sh
@@ -6,20 +6,21 @@ AC=${4:?action}
 OUT=${5:?out}
 START=20000
 END=$(expr $START + $N - 1)
-ID="$(basename $RT)-$AC"
 # starting images
 seq $START $END | while read port 
 do docker run -d -p $port:8080  --name "under-test-$port" --rm $RT
 done
 # testing
 sleep 1
-seq $START $END | xargs time ./multipost -init $AC -run run.dat 2>$ID.init 
+echo "init $(basename $RT) $AC $N" >>$OUT
+seq $START $END | xargs time ./multipost -init $AC -run run.dat 2>>$OUT 
 sleep 1
-echo $START | xargs time ./multipost -run run.dat -repeat $NN 2>$ID.run
-INIT=$(awk '{print $1}' <$ID.init)
-RUN=$(awk '{print $1}' <$ID.run)
-echo  $(printf "%05d init %05.2f" $N $INIT) $RT $AC >>$OUT
-echo  $(printf "%05d run_ %05.2f" $NN $RUN) $RT $AC>>$OUT
+echo "run $(basename $RT) $AC $NN" >>$OUT
+echo $START | xargs time ./multipost -run run.dat -repeat $NN 2>>$OUT
+#INIT=$(awk '{print $1}' <$ID.init)
+#RUN=$(awk '{print $1}' <$ID.run)
+#echo  $(printf "%05d init %05.2f" $N $INIT) $RT $AC >>$OUT
+#echo  $(printf "%05d run_ %05.2f" $NN $RUN) $RT $AC>>$OUT
 # stopping images
 seq $START $END | while read port 
 do docker kill "under-test-$port" 
