7d4d30c756f6499a69866bef179a2882527bc793
Michele Sciabarra <sciabarracom@sciabarra.com>
2018-12-16 11:38:52 +0100

    fixed tests form python3.7

--- python3.7/pythonbuild.py
+++ python3.7/pythonbuild.py
@@ -17,6 +17,7 @@
 # limitations under the License.
 #
 """
+
 from __future__ import print_function
 import os
 import re
@@ -24,33 +25,62 @@ import sys
 import codecs
 import subprocess
 
+
+# The crazy logic of this assembling:
+# - a single exec is renamed to __main__.py
+# - otherwise if there is a __main__ and an exec, the __main__ win
+# - the __main__ is copied to main___
+# - if there is a "if __name__ == __main__" it is executed
+# otherwise a launcher is created 
 def sources(launcher, source_dir, main):
+    # source and dest
     src = "%s/exec" % source_dir
-    dst = "%s/exec__.py" % source_dir 
+    dst = "%s/__main__.py" % source_dir 
+    # copy exec to __main__
+    # detect if it is also an entry point
+    body = ""
     if os.path.isfile(src) and not os.path.isfile(dst):
-        body = ""
         with codecs.open(src, 'r', 'utf-8') as s:
             body = s.read()
         with codecs.open(dst, 'w', 'utf-8') as d:
             d.write(body)
 
-
-    launcher_file = "%s/main__.py" % source_dir
-    with codecs.open(launcher_file, 'w', 'utf-8') as d:
-        with codecs.open(launcher, 'r', 'utf-8') as s:
+    # renaming __main__ to main__
+    has_main = False
+    body = ""
+    src = "%s/__main__.py" % source_dir
+    dst = "%s/main__.py" % source_dir 
+    if os.path.isfile(src):
+        with codecs.open(src, 'r', 'utf-8') as s:
             body = s.read()
-            body.replace("from exec__ import main as main", 
-                         "from exec__ import %s as main" % main)
+        has_main = body.find("""if __name__ == '__main__':""") != -1
+        with codecs.open(dst, 'w', 'utf-8') as d:
             d.write(body)
 
-def build(source_dir, target_file):
+    # we have a main, we can return
+    if has_main:
+        return dst
+
+    # copy a launcher
+    starter = "%s/exec__.py" % source_dir
+    with codecs.open(launcher, 'r', 'utf-8') as s:
+        with codecs.open(starter, 'w', 'utf-8') as d:
+            body = s.read()
+            body = body.replace("from main__ import main as main", 
+                         "from main__ import %s as main" % main)
+            d.write(body)
+    return starter
+
+def build(source_dir, target_file, launcher):
     with codecs.open(target_file, 'w', 'utf-8') as d:
         d.write("""#!/bin/bash
 cd %s
-exec python3 main__.py
-""" % source_dir)
+if test -d virtualenv
+then export PYTHONPATH="$PWD/virtualenv"
+fi
+exec python3 %s
+""" % (source_dir, launcher))
     os.chmod(target_file, 0o755)
-
 
 def main(argv):
     if len(argv) < 4:
@@ -61,8 +91,8 @@ def main(argv):
     source_dir = os.path.abspath(argv[2])
     target_file = os.path.abspath("%s/exec" % argv[3])
     launcher = os.path.abspath(argv[0]+".launcher.py")
-    sources(launcher, source_dir, main)
-    build(source_dir, target_file)
+    starter = sources(launcher, source_dir, main)
+    build(source_dir, target_file, starter)
 
 if __name__ == '__main__':
     main(sys.argv)
--- python3.7/pythonbuild.py.launcher.py
+++ python3.7/pythonbuild.py.launcher.py
@@ -1,17 +1,27 @@
+from __future__ import print_function
 from sys import stdin
+from sys import stdout
+from sys import stderr
 from os import fdopen
-import json
-from exec__ import main as main
+import os, json
+from main__ import main as main
 
-out = fdopen(3, "w")
+out = fdopen(3, "wb")
+env = os.environ
 while True:
   line = stdin.readline()
   if not line: break
   args = json.loads(line)
   payload = {}
-  if "value" in args:
-    payload = args["value"]
+  for key in args:
+    if key == "value":
+      payload = args["value"]
+    else:
+      env["__OW_%s" % key.upper()]= args[key]
   res = main(payload)
-  print(json.dumps(res), file=out)
+  out.write(json.dumps(res, ensure_ascii=False).encode('utf-8'))
+  out.write(b'\n')
+  stdout.flush()
+  stderr.flush()
   out.flush()
 
