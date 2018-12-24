144a0bf135f44eafb48dfcdbe68da1b5bfc3ab10
Michele Sciabarra <michele@sciabarra.com>
2018-12-24 10:22:49 +0100

    updated benchmarks

--- benchmark/test.sh
+++ benchmark/test.sh
@@ -4,8 +4,10 @@ NN=${2:?count init}
 RT=${3:?runtime}
 AC=${4:?action}
 OUT=${5:?out}
-START=20000
+START=9000
 END=$(expr $START + $N - 1)
+# killing 
+docker ps -q | xargs docker kill
 # starting images
 seq $START $END | while read port 
 do docker run -d -p $port:8080  --name "under-test-$port" --rm $RT
@@ -13,7 +15,7 @@ done
 # testing
 sleep 1
 echo "init $(basename $RT) $AC $N" >>$OUT
-seq $START $END | xargs time ./multipost -init $AC -run run.dat 2>>$OUT 
+seq $START $END | xargs /usr/bin/time -p ./multipost -init $AC -run run.dat 2>>$OUT 
 sleep 1
 # stopping images
 seq $(expr $START + 1) $END | while read port 
@@ -21,7 +23,7 @@ do docker kill "under-test-$port"
 done
 # testing run
 echo "run $(basename $RT) $AC $NN" >>$OUT
-echo $START | xargs time ./multipost -run run.dat -repeat $NN 2>>$OUT
+echo $START | xargs /usr/bin/time -p ./multipost -run run.dat -repeat $NN 2>>$OUT
 docker kill under-test-$START
 cat $OUT
 
